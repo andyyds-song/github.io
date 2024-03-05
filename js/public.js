2f917243ca112249fdc04220827d460e599f0c09
andyyyds <34959818+andyyds-song@users.noreply.github.com>
2024-03-05 12:12:21 +0800

    Update public.js
    Signed-off-by: andyyyds <34959818+andyyds-song@users.noreply.github.com>

--- js/public.js
+++ js/public.js
@@ -1,5 +1,6 @@
 const JSONList = [
     { id: "2023", name: '测试', describe: '基础必背，简答题，', file: 'a.json'},
+    { id: "2023", name: '初二(下)Module-1', describe: '基础必背，简答题，', file: '初二下Module1.json'},
     { id: "2023", name: '初二(上)Module-1', describe: '基础必背，简答题，', file: '初二上Module1.json'},
     { id: "2023", name: '初二(上)Module-2', describe: '基础必背，简答题，', file: '初二上Module2.json'},
     { id: "2023", name: '初二(上)Module-3', describe: '基础必背，简答题，', file: '初二上Module3.json'},
@@ -12,7 +13,6 @@ const JSONList = [
     { id: "2023", name: '初二(上)Module-10', describe: '基础必背，简答题，', file: '初二上Module10.json'},
     { id: "2023", name: '初二(上)Module-11', describe: '基础必背，简答题，', file: '初二上Module11.json'},
     { id: "2023", name: '初二(上)Module-12', describe: '基础必背，简答题，', file: '初二上Module12.json'},
-    { id: "2023", name: '初二(下)Module-1', describe: '基础必背，简答题，', file: '初二下Module1.json'},
     { id: "2023", name: '初二历史第一次鸦片战争', describe: '题试卷，简答题，', file: "第一次鸦片战争.json" },
     { id: "2023", name: 'CAD快捷指令练习', describe: '题试卷，包含选择和简答题，', file: "cad.json" },
     { id: "2023", name: '单词的过去式', describe: '初一下册过去式练习', file: "过去式.json" },
